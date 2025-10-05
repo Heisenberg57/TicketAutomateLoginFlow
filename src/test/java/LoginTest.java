37b344d00c5d687fdec9d7ed62f7c4fcf2599a67
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-10-05 18:35:54 +0530

    first commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,41 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+public class LoginTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("--password-store=basic");
+        options.addArguments("--no-default-browser-check");
+        options.addArguments("--disable-features=PasswordLeakDetection");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        Thread.sleep(3000);
+
+
+        try {
+            Assert.assertEquals("Swag Labs", driver.findElement(By.className("app_logo")).getText(), "Login succesfull");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+
+}
